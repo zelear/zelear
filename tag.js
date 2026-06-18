b369d7bc2f945ff0fc03287aba733768f59fb393
ZelearFox <140787513+zelear@users.noreply.github.com>
2026-06-18 23:54:50 +0500

    Create tag.js

---
+++ tag.js
@@ -0,0 +1,10 @@
+{
+    "ru": {
+        "text": "Сделано ZelearFox 🦊",
+        "link": "https://github.com/zelear/"
+    },
+    "en": {
+        "text": "Made by ZelearFox 🦊",
+        "link": "https://github.com/zelear/"
+    }
+}
